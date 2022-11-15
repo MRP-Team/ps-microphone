6515a41a43919089744d92e3fc9dce318d9757a4
abdel1touimi <abdel1.touimi@gmail.com>
2022-11-15 21:46:46 +0100

    fixed createuseableitem

--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,5 @@
-exports[Config.Inventory]:CreateUsableItem("megaphone", function(source,item)
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("megaphone", function(source,item)
     TriggerClientEvent("megaphone:Toggle",source)
 end)
